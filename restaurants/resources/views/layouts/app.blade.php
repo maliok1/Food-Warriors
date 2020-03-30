9c94582f95a66ca8929178311a27bffebd9b4047
Michaela <59257576+Misjak@users.noreply.github.com>
2020-03-30 11:24:41 +0200

    styling

--- restaurants/resources/views/layouts/app.blade.php
+++ restaurants/resources/views/layouts/app.blade.php
@@ -25,6 +25,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet">
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
--- restaurants/resources/views/restaurants/index.blade.php
+++ restaurants/resources/views/restaurants/index.blade.php
@@ -1,28 +1,42 @@
 @extends ('layouts.app')
 
-
-
 @section('list of restaurants') 
 
    <!-- Go to your restaurant button-->
-   
-  @auth 
-    @foreach($restaurants as $restaurant)
-      @if(auth()->user()->id === $restaurant->user_id)
-             <a href="restaurant/{{$restaurant->id}}">Go to my restaurant</a>
-      @endif
-    @endforeach 
-  @endauth
 
-    <h1>Restaurants:</h1>
+<div class="m-4" style="font-family: 'Roboto', sans-serif">
+      @auth 
+        @foreach($restaurants as $restaurant)
+          @if(auth()->user()->id === $restaurant->user_id)
+            <a class="m-2 btn btn-dark" href="restaurant/{{$restaurant->id}}">Go to my restaurant</a>
+          @endif
+        @endforeach 
+      @endauth
 
-    @foreach($restaurants as $restaurant)
-      <a href="/restaurant/{{$restaurant->id}}">{{$restaurant->name}}</a>
-      <h5>{{$restaurant->city}}</h5>
-      @if($restaurant->image)
-        <img src="{{$restaurant->image}}" alt="{{$restaurant->name}}"> 
-      @endif
-     <br>
-    @endforeach
-@endsection
+    <!-- List of restaurants -->
 
+      <h1 class="m-2">Restaurants</h1>
+        <hr>
+          @foreach($restaurants as $restaurant)
+            <div class="d-inline-flex m-2">
+              <div class="align-self-center">
+                <div class="card" style="width: 20rem; border-radius: 15px">
+                  @if($restaurant->image)
+                    <img class="card-img-top" style="border-radius: 15px" src="{{$restaurant->image}}" alt="{{$restaurant->name}}">
+                  @endif
+                <div class="card-body">
+                  <a href="/restaurant/{{$restaurant->id}}"><h2>{{$restaurant->name}}</h2></a>
+                  <h5 class="card-title">{{$restaurant->city}}</h5>
+                  <p class="card-text"></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        @endforeach
+    @endsection
+</div>
+
+{{-- for overlaping 
+<div class="card-img-overlay">
+<h4 class="card-title">John Doe</h4>
+<p class="card-text">Some example text.</p> --}}
