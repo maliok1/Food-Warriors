ea0df6466df0c9000ad1e79b7474a71d3889d5bd
Anastasia Mal <lisanastya15@gmail.com>
2020-04-01 19:33:27 +0200

    improved ui of the sserch bar in the mobile v

--- restaurants/resources/views/layouts/app.blade.php
+++ restaurants/resources/views/layouts/app.blade.php
@@ -69,16 +69,13 @@ footer {
                         <div class="search-area">
                             <input class="form-control form-control-sm" type="text" placeholder="Search"
                                 aria-label="Search" name="q">
-
-                            <button type="submit" class="btn btn-default" style="padding: 0 5px">
+                            <button type="submit" class="btn btn-default" style="padding: 0 5x">
                                 <svg class="bi bi-search" width="1em" height="1em" viewBox="0 0 18 18" fill="currentColor" xmlns="http://www.w3.org/2000/svg" color="whitesmoke">
                                 <path fill-rule="evenodd" d="M10.442 10.442a1 1 0 011.415 0l3.85 3.85a1 1 0 01-1.414 1.415l-3.85-3.85a1 1 0 010-1.415z" clip-rule="evenodd"/>
                                 <path fill-rule="evenodd" d="M6.5 12a5.5 5.5 0 100-11 5.5 5.5 0 000 11zM13 6.5a6.5 6.5 0 11-13 0 6.5 6.5 0 0113 0z" clip-rule="evenodd"/>
                                 </svg>
                             </button>
                         </div>
-                            
-                                
                     </form>
                     </ul>
 
