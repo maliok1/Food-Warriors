48751bae944a66db4c80a99e70d876e1d0f9110b
Michaela <59257576+Misjak@users.noreply.github.com>
2020-04-01 15:38:09 +0200

    boo

--- restaurants/public/js/app.js
+++ restaurants/public/js/app.js
@@ -9756,7 +9756,7 @@ exports.push([module.i, "@import url(https://fonts.googleapis.com/css?family=Lat
 exports.push([module.i, "@import url(https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css);", ""]);
 
 // module
-exports.push([module.i, "a {\r\n    text-decoration: none;\r\n    color: #fff;\r\n}\r\n\r\na:hover {\r\n    text-decoration: none;\r\n}\r\n\r\nul {\r\n    padding:0;\r\n    list-style: none;\r\n}\r\n.route {\r\n    display: flex;\r\n    flex-wrap: wrap;\r\n\r\n}\r\n.footer-copyright {\r\n    font-family: 'Lato', sans-serif;\r\n    color: #FFF;\r\n    background: #322f30;\r\n \r\n}\r\n\r\n.footer-social-icons {\r\n    font-family: 'Lato', sans-serif;\r\n    color: #FFF;\r\n    background: #322f30;\r\n    -webkit-font-smoothing: antialiased;\r\n    width: 100%;\r\n    display: inline-flex;\r\n    flex-wrap: wrap;\r\n    justify-content: space-between ;\r\n    margin: 0 0;\r\n}\r\n@media (max-width: 600px) { \r\n    .footer-social-icons {\r\n        display: flex;\r\n        justify-content: center;\r\n        width: 100%;\r\n    }\r\n}\r\n.social-icon {\r\n    color: #fff;\r\n    font-size: 2em;\r\n}\r\n\r\nul.social-icons {\r\n    margin-top: 10px;\r\n}\r\n.social-icons li {\r\n    vertical-align: top;\r\n    display: inline;\r\n}\r\n.social-icons a {\r\n    color: #fff;\r\n    text-decoration: none;\r\n}\r\n.fa-facebook {\r\n    padding:10px 19px;\r\n    transition: .5s;\r\n    background-color: #322f30;\r\n}\r\n.fa-facebook:hover {\r\n    background-color: #3d5b99;\r\n}\r\n.fa-twitter {\r\n    padding:10px 12px;\r\n    transition: .5s;\r\n    background-color: #322f30;\r\n}\r\n.fa-twitter:hover {\r\n    background-color: #00aced;\r\n}\r\n\r\n.fa-linkedin {\r\n    padding:10px 14px;\r\n    transition: .5s;\r\n    background-color: #322f30;\r\n}\r\n.fa-linkedin:hover {\r\n    background-color: #0073a4;\r\n}\r\n.fa-github {\r\n    margin-right: 7px;\r\n    padding:10px 14px;\r\n    transition: .5s;\r\n    background-color: #322f30;\r\n}\r\n.fa-github:hover {\r\n    background-color: #5a32a3;\r\n}", ""]);
+exports.push([module.i, "a {\n    text-decoration: none;\n    color: #fff;\n}\n\na:hover {\n    text-decoration: none;\n}\n\nul {\n    padding:0;\n    list-style: none;\n}\n.route {\n    display: flex;\n    flex-wrap: wrap;\n\n}\n.footer-copyright {\n    font-family: 'Lato', sans-serif;\n    color: #FFF;\n    background: #322f30;\n \n}\n\n.footer-social-icons {\n    font-family: 'Lato', sans-serif;\n    color: #FFF;\n    background: #322f30;\n    -webkit-font-smoothing: antialiased;\n    width: 100%;\n    display: inline-flex;\n    flex-wrap: wrap;\n    justify-content: space-between ;\n    margin: 0 0;\n}\n@media (max-width: 600px) { \n    .footer-social-icons {\n        display: flex;\n        justify-content: center;\n        width: 100%;\n    }\n}\n.social-icon {\n    color: #fff;\n    font-size: 2em;\n}\n\nul.social-icons {\n    margin-top: 10px;\n}\n.social-icons li {\n    vertical-align: top;\n    display: inline;\n}\n.social-icons a {\n    color: #fff;\n    text-decoration: none;\n}\n.fa-facebook {\n    padding:10px 19px;\n    transition: .5s;\n    background-color: #322f30;\n}\n.fa-facebook:hover {\n    background-color: #3d5b99;\n}\n.fa-twitter {\n    padding:10px 12px;\n    transition: .5s;\n    background-color: #322f30;\n}\n.fa-twitter:hover {\n    background-color: #00aced;\n}\n\n.fa-linkedin {\n    padding:10px 14px;\n    transition: .5s;\n    background-color: #322f30;\n}\n.fa-linkedin:hover {\n    background-color: #0073a4;\n}\n.fa-github {\n    margin-right: 7px;\n    padding:10px 14px;\n    transition: .5s;\n    background-color: #322f30;\n}\n.fa-github:hover {\n    background-color: #5a32a3;\n}", ""]);
 
 // exports
 
@@ -9775,7 +9775,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "\r\n.person-wrapper {\r\n    margin-bottom: 2em;\r\n}\r\n\r\n.profile-pic {\r\n    \r\n    width: 70%;\r\n    border-radius: 50%;\r\n    margin-bottom: 1em;\r\n    margin-top: 3em;\r\n\r\n}\r\n.profile-pic:hover {\r\n    transition: .5s;\r\n    width: 90%;\r\n    border-radius: 50%;\r\n\r\n}\r\n\r\n\r\nh3 {\r\n    margin-bottom: 1em;\r\n}\r\n\r\n/* @media (max-height:800px){\r\n    footer { position: static; }\r\n} */", ""]);
+exports.push([module.i, "\n.person-wrapper {\n    margin-bottom: 2em;\n}\n\n.profile-pic {\n    \n    width: 70%;\n    border-radius: 50%;\n    margin-bottom: 1em;\n    margin-top: 3em;\n\n}\n.profile-pic:hover {\n    transition: .5s;\n    width: 90%;\n    border-radius: 50%;\n\n}\n\n\nh3 {\n    margin-bottom: 1em;\n}\n\n/* @media (max-height:800px){\n    footer { position: static; }\n} */", ""]);
 
 // exports
 
@@ -9794,7 +9794,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, "@media (max-width: 600px) { \r\n    .container {\r\n        display: flex;\r\n        justify-content: space-around;\r\n        width: 90%;\r\n    }\r\n}", ""]);
+exports.push([module.i, "@media (max-width: 600px) { \n    .container {\n        display: flex;\n        justify-content: space-around;\n        width: 90%;\n    }\n}", ""]);
 
 // exports
 
@@ -9813,7 +9813,7 @@ exports = module.exports = __webpack_require__(/*! ../../../../node_modules/css-
 
 
 // module
-exports.push([module.i, ".container {\r\n    display: flex;\r\n    justify-content: space-between;\r\n}\r\n\r\n@media (max-width: 600px) { \r\n    .container {\r\n        display: flex;\r\n        justify-content: space-around;\r\n    }\r\n}", ""]);
+exports.push([module.i, ".container {\n    display: flex;\n    justify-content: space-between;\n}\n\n@media (max-width: 600px) { \n    .container {\n        display: flex;\n        justify-content: space-around;\n    }\n}", ""]);
 
 // exports
 
@@ -86745,8 +86745,8 @@ if (document.getElementById('news')) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Web\food-warriors\restaurants\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Web\food-warriors\restaurants\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/Misjak/WEB/Exercises/Final project/Food-Warriors/restaurants/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/Misjak/WEB/Exercises/Final project/Food-Warriors/restaurants/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
