8999501e8f36746a369a8927d2c3d6dabcecc207
Anastasia Mal <lisanastya15@gmail.com>
2020-04-03 15:24:05 +0200

    search area has changed

--- restaurants/resources/views/restaurants/search-results.blade.php
+++ restaurants/resources/views/restaurants/search-results.blade.php
@@ -6,21 +6,10 @@
           <p> The Search results for your query <b> {{ $query }} </b> are :</p>
       <h2>Restaurants you have been searching for:</h2>
       <table class="search-results-table">
-          <thead>
-              <tr>
-                  <th>Name</th>
-                  <th>City</th>
-                  <th>Description</th>
-              </tr>
-          </thead>
-          <tbody>
               @foreach($details as $restaurant)
-              <tr>
-                  <td class="search-result-name"><a href="/restaurant/{{$restaurant->id}}">{{$restaurant->name}}</a></td>
-                  <td class="search-result-city">{{$restaurant->city}}</td>
-                  <td class="search-result-description">{{$restaurant->description}}</td>
-                  <td><img class="search-result-img" src="{{$restaurant->image}}" alt="{{$restaurant->name}}"></td>
-              </tr>
+                  <a class="search-result-name" href="/restaurant/{{$restaurant->id}}">{{$restaurant->name}}</a>
+
+                  <img class="search-result-img" src="{{$restaurant->image}}" alt="{{$restaurant->name}}">
               @endforeach
           </tbody>
       </table>
